49e9831f36f4d10f007993bd41512d5ec6aa2843
tamyressilvazz <74621120+tamyressilvazz@users.noreply.github.com>
2021-09-20 15:58:42 -0300

    add o construtor init

--- 02.py
+++ 02.py
@@ -1,19 +1,27 @@
 '''Ler nome e salário de dois funcionários, mostrar o salário médio dos funcionários'''
 
 class Funcionarios:
-    #Dados funcionário 01:
-    print('Dados do primeiro funcionário: ')
-    nome_01 = input('Nome: ')
-    salario_01 = float(input('Salário: '))
 
-    #Dados funcionário 02:
-    print('Dados do segundo funcionário: ')
-    nome_02 = input('Nome: ')
-    salario_02 = float(input('Salário: '))
+    def __init__(self, sum_salarios, media_salarial):
+        self.sum_salarios = sum_salarios
+        self.media_salarial = media_salarial
 
-    sum_salarios = sum([salario_01, salario_02])
+    def mediaSalarial(self):
+        self.sum_salarios = sum([salario_01, salario_02])
+        self.media_salarial = self.sum_salarios/2
 
-    media_salarial = sum_salarios/2
+        return self.media_salarial
 
-    print(f'Salário médio = {media_salarial}')
+#Dados funcionário 01:
+print('Dados do primeiro funcionário: ')
+nome_01 = input('Nome: ')
+salario_01 = float(input('Salário: '))
 
+#Dados funcionário 02:
+print('Dados do segundo funcionário: ')
+nome_02 = input('Nome: ')
+salario_02 = float(input('Salário: '))
+
+funcionarios = Funcionarios(None, None)
+
+print(f'Salário médio = {funcionarios.mediaSalarial()}')
